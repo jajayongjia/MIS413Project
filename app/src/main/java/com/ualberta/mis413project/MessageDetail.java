de2526a3bff0871a63af210677fbdf854fb18955
jajayongjia <yongjiahuang@Yongjias-MacBook-Pro.local>
2017-11-29 17:55:53 -0700

    finish vesrsion 3

--- app/src/main/java/com/ualberta/mis413project/MessageDetail.java
+++ app/src/main/java/com/ualberta/mis413project/MessageDetail.java
@@ -1,7 +1,10 @@
 package com.ualberta.mis413project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MessageDetail extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MessageDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_detail);
         this.setTitle("Message Dertail");
+        Button replayButton = (Button) findViewById(R.id.button3);
+        replayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+
+        });
+
     }
 }
